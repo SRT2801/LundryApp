31c34d37b9b144b2688fe5429f2c403982bc40bb
SRT DEV <sahirruiz4@gmail.com>
2025-04-13 21:21:31 -0500

    Actualizar rutas en TabsPage para evitar duplicación de segmentos

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,7 +4,7 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: '', // Cambiado de 'tabs' a '' para evitar la duplicación de segmentos
     component: TabsPage,
     children: [
       {
@@ -25,15 +25,11 @@ const routes: Routes = [
         pathMatch: 'full'
       }
     ]
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full'
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class TabsPageRoutingModule {}
