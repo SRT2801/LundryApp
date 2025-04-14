b4f63495b86b4cd358b93283a1e0413398e780d0
SRT DEV <sahirruiz4@gmail.com>
2025-04-13 21:21:03 -0500

    Agregar componentes personalizados a SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
+import { CustomButtonComponent } from './components/custom-button/custom-button.component';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CustomInputComponent,
+    CustomButtonComponent
+  ],
+  exports: [
+    CustomInputComponent,
+    CustomButtonComponent
   ]
 })
 export class SharedModule { }
